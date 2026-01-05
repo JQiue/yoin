fe3bac91f87ea53cad79e9a0205090ec78d25169
JQiue <861947542@qq.com>
2026-01-05 22:27:37 +0800

    feat: implement basic Axum routing
    Closes #4

--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,22 @@ fn default_database_url() -> String {
   "sqlite://./yoin.sqlite?mode=rwc".to_string()
 }
 
+fn default_host() -> String {
+  "127.0.0.1".to_string()
+}
+
+fn default_port() -> u16 {
+  7410
+}
+
 #[derive(Deserialize)]
 pub struct Config {
   #[serde(default = "default_database_url")]
   pub database_url: String,
+  #[serde(default = "default_host")]
+  pub host: String,
+  #[serde(default = "default_port")]
+  pub port: u16,
 }
 
 impl Config {
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,56 @@
-use tracing::level_filters::LevelFilter;
-use tracing_subscriber::{EnvFilter, filter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use axum::{Json, Router, response::IntoResponse, routing::get};
+use serde_json::json;
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing::info;
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
-use crate::db::migrate;
+use crate::{config::Config, db::migrate};
 
 mod config;
 mod db;
 mod entity;
 
+#[derive(Clone)]
+struct AppState {}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
   init_log();
-  let config = config::Config::from_env().unwrap();
+  let config = Config::from_env().unwrap();
   let conn = migrate(&config.database_url).await.unwrap();
-  conn.ping().await.unwrap();
+  let addr = format!("{}:{}", config.host, config.port);
+  let state = AppState {};
+  info!("listening on http://{}", addr);
+  app(&addr, router(state)).await;
+  Ok(())
 }
 
 fn init_log() {
-  let target_filter = filter::Targets::new()
-    .with_default(LevelFilter::TRACE)
-    .with_target("sqlx::query", LevelFilter::OFF)
-    .with_target("html5ever", LevelFilter::OFF)
-    .with_target("rustls", LevelFilter::OFF);
-  let env_filter = EnvFilter::try_from_default_env()
-    .or_else(|_| EnvFilter::try_new("info"))
-    .unwrap();
   tracing_subscriber::registry()
-    .with(tracing_subscriber::fmt::layer().with_timer(fmt::time::LocalTime::rfc_3339()))
-    .with(target_filter)
-    .with(env_filter)
+    .with(
+      tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("debug,sqlx::query=off,hyper=debug,axum=debug")),
+    )
+    .with(tracing_subscriber::fmt::layer().with_target(false))
     .init();
+}
+
+async fn app(addr: &str, router: Router) {
+  axum::serve(TcpListener::bind(addr).await.unwrap(), router)
+    .await
+    .unwrap();
+}
+
+fn router(state: AppState) -> Router {
+  Router::new()
+    .nest("/api/v1", Router::new().route("/health", get(health_check)))
+    .layer(TraceLayer::new_for_http())
+    .with_state(state)
+}
+
+async fn health_check() -> impl IntoResponse {
+  Json(json!({
+    "status": "ok"
+  }))
 }
