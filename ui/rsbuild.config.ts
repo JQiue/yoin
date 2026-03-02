ed1b0ca8e634c2bc7244341bf57331a2e5ac769b
JQiue <861947542@qq.com>
2026-03-02 13:38:12 +0800

    feat: add biome configuration

--- ui/rsbuild.config.ts
+++ ui/rsbuild.config.ts
@@ -1,46 +1,45 @@
 import { defineConfig } from "@rsbuild/core";
-import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginPreact } from "@rsbuild/plugin-preact";
 export default defineConfig({
-  plugins: [pluginPreact()],
-  dev: {
-    assetPrefix: "/",
-  },
-  performance: {
-    chunkSplit: {
-      strategy: "all-in-one",
-    },
-    bundleAnalyze: {
-      generateStatsFile: true,
-    },
-  },
-  output: {
-    injectStyles: true,
-    filenameHash: false,
-    target: "web",
-    distPath: {
-      js: "",
-      css: "",
-    },
-    copy: [
-      {
-        from: "./src/index.d.ts",
-        to: "./index.d.ts",
-      },
-    ],
-  },
-  html: {
-    template: "./src/index.html",
-  },
-  tools: {
-    rspack: {
-      output: {
-        library: {
-          name: "Yoin",
-          type: "window",
-          export: "default",
-        },
-      },
-    },
-  },
+	plugins: [pluginPreact()],
+	dev: {
+		assetPrefix: "/",
+	},
+	performance: {
+		chunkSplit: {
+			strategy: "all-in-one",
+		},
+		// bundleAnalyze: {
+		//   generateStatsFile: true,
+		// },
+	},
+	output: {
+		injectStyles: true,
+		filenameHash: false,
+		target: "web",
+		distPath: {
+			js: "",
+			css: "",
+		},
+		copy: [
+			{
+				from: "./src/index.d.ts",
+				to: "./index.d.ts",
+			},
+		],
+	},
+	html: {
+		template: "./src/template/index.html",
+	},
+	tools: {
+		rspack: {
+			output: {
+				library: {
+					name: "Yoin",
+					type: "window",
+					export: "default",
+				},
+			},
+		},
+	},
 });
